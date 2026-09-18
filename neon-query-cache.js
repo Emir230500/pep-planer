b225bc447acbe87aa3200f25786d2eef10445c31
Emir230500 <emirkandemircan@gmx.de>
2026-09-18 11:24:40 +0200

    Extend Neon query cache to seven days

--- neon-query-cache.js
+++ neon-query-cache.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const DEFAULT_TTL_MS = 24 * 60 * 60 * 1000;
+const DEFAULT_TTL_MS = 7 * 24 * 60 * 60 * 1000;
 const configuredTtl = Number(process.env.NEON_QUERY_CACHE_TTL_MS || DEFAULT_TTL_MS);
 const CACHE_TTL_MS = Number.isFinite(configuredTtl) && configuredTtl >= 5 * 60 * 1000
   ? configuredTtl
