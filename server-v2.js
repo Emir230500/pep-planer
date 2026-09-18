315d09b726b9a73f47e9594d3df8c0ac397b991f
Emir230500 <emirkandemircan@gmx.de>
2026-09-18 16:27:34 +0200

    Use compact bakery forecasting runtime

---
+++ server-v2.js
@@ -0,0 +1,5 @@
+"use strict";
+const fs=require("fs"),path=require("path");
+let code="";
+for(let i=1;i<=4;i++)code+=fs.readFileSync(path.join(__dirname,`server-v2.part${i}`),"utf8");
+new Function("require","__dirname","__filename","module","exports",code)(require,__dirname,__filename,module,exports);
