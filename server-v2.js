33b47c33487b34221499052c25a42c30ab251e75
Emir230500 <emirkandemircan@gmx.de>
2026-09-18 16:35:08 +0200

    Load existing bakery history seed

--- server-v2.js
+++ server-v2.js
@@ -1,5 +1,5 @@
 "use strict";
 const fs=require("fs"),path=require("path");
 let code="";
-for(let i=1;i<=4;i++)code+=fs.readFileSync(path.join(__dirname,`server-v2.part${i}`),"utf8");
+for(let i=1;i<=5;i++)code+=fs.readFileSync(path.join(__dirname,`server-v2.part${i}`),"utf8");
 new Function("require","__dirname","__filename","module","exports",code)(require,__dirname,__filename,module,exports);
